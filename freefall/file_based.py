31ef66f9bd67d5c9eb15fab0e129f41f940ab7bc
Kazuma Takahara <4269kzm@gmail.com>
2018-06-18 02:27:30 +0900

    v3.0

---
+++ freefall/file_based.py
@@ -0,0 +1,36 @@
+import json
+from abc import ABCMeta
+from pathlib import Path
+
+import filelock
+
+from .base import BaseDownloader
+
+
+class FileBasedDownloader(BaseDownloader, metaclass=ABCMeta):
+    def __init__(self):
+        self._filelock = {}
+
+    def _lock_status(self, resource):
+        path = str(self._filelock_path(resource))
+        if path not in self._filelock:
+            Path(path).parent.mkdir(parents=True, exist_ok=True)
+            self._filelock[path] = filelock.FileLock(path)
+        return self._filelock[path]
+
+    def _load_status(self, resource):
+        try:
+            with open(str(self._status_path(resource))) as fp:
+                return json.load(fp)
+        except FileNotFoundError:
+            return {}
+
+    def _save_status(self, resource, status):
+        with open(str(self._status_path(resource)), 'w') as fp:
+            json.dump(status, fp)
+
+    def _status_path(self, resource):
+        return Path(self._archive_prefix(resource), '.status.json')
+
+    def _filelock_path(self, resource):
+        return Path(self._archive_prefix(resource), '.lock')
