d68cfa0c1a9a7965e3de9ead1f33b3caf64df504
Kazuma Takahara <4269kzm@gmail.com>
2018-07-27 23:27:31 +0900

    v8.2.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('LICENSE') as f:
 
 setup(
     name='freefall',
-    version='8.2.1',
+    version='8.2.2',
     description='Simple downloader',
     long_description=README,
     author='kzm4269',
